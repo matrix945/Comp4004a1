ba5335e95e20d0f50315e0fba643a1595392fadd
matrix945 <frankzhang945@gmail.com>
2018-10-09 01:48:39 -0400

    java gb collection :(
    readCard()
    array empty after clear

--- src/Poker.java
+++ src/Poker.java
@@ -7,105 +7,91 @@ import java.util.Collections;
 
 public class Poker {
 
-    public static boolean checkStraight(ArrayList<Card> t){
+    public static boolean checkStraight(ArrayList<Card> t) {
 
         //check the suit not same suit
-        for (int i=0;i<5;i++) {
+        for (int i = 0; i < 5; i++) {
             if (t.get(0).suit != t.get(i).suit) {
                 return false;
             }
         }
 
         //bug for handle A 13 12 11 10 9
-        for (int i = 0;i<5;i++){
-            if (t.get(0).rank != t.get(i).rank+i) {
+        for (int i = 0; i < 5; i++) {
+            if (t.get(0).rank != t.get(i).rank + i) {
                 return false;
             }
         }
 
 
-
-
-
-    return true;
+        return true;
     }
 
 
+    public boolean win(int a, int b) {
+
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+        return (a > b);
+
+    }
 
 
+    public static void readCard() {
+        ArrayList allGames = new ArrayList();
 
 
-	public boolean win (int a , int b ) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Mao\\eclipse-workspace\\Comp4004a1\\data"))) {
+            String line;
+            String[] tempArray;
+            while ((line = br.readLine()) != null) {
+                ArrayList oneGames = new ArrayList();
+                ArrayList<Card> hand = new ArrayList<Card>();
+                ArrayList<Card> init = new ArrayList<Card>();
+                ArrayList<Card> exchange = new ArrayList<Card>();
 
-		System.out.println("a is " + a);
-		System.out.println("b is " + b);
-		return (a>b);
+                System.out.println(line);
+                tempArray = line.split(" ");
 
-	}
-
-
-	public static void readCard() {
-		ArrayList allGames = new ArrayList();
-		ArrayList oneGames = new ArrayList();
-
-		ArrayList<Card> hand = new ArrayList<Card> ();
-		ArrayList<Card> init = new ArrayList<Card> ();
-		ArrayList<Card> exchange = new ArrayList<Card> ();
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Mao\\eclipse-workspace\\Comp4004a1\\data"))) {
-		    String line;
-		    String[] tempArray;
-		    while ((line = br.readLine()) != null) {
-		       System.out.println(line);
-		       tempArray = line.split(" ");
-
-
-
-		       for (int i =0 ;i<5;i++) {
-                   Card a = new Card();
-                   a.suit = tempArray[i].charAt(0);
-                   a.rank = convert(tempArray[i]);
-                   hand.add(a);
-		       }
-		       for (int i =5 ;i<10;i++) {
-                   Card a = new Card();
-                   a.suit = tempArray[i].charAt(0);
-                   a.rank = convert(tempArray[i]);
-                   init.add(a);
-		       }
-                for (int i =10 ;i<12;i++) {
+                for (int i = 0; i < 5; i++) {
+                    Card a = new Card();
+                    a.suit = tempArray[i].charAt(0);
+                    a.rank = convert(tempArray[i]);
+                    hand.add(a);
+                }
+                for (int i = 5; i < 10; i++) {
+                    Card a = new Card();
+                    a.suit = tempArray[i].charAt(0);
+                    a.rank = convert(tempArray[i]);
+                    init.add(a);
+                }
+                for (int i = 10; i < 12; i++) {
                     Card a = new Card();
                     a.suit = tempArray[i].charAt(0);
                     a.rank = convert(tempArray[i]);
                     exchange.add(a);
                 }
-
-
                 Collections.sort(hand, Card::compareTo);
                 Collections.sort(init, Card::compareTo);
                 Collections.sort(exchange, Card::compareTo);
-		       oneGames.add(hand);
-		       oneGames.add(init);
-		       oneGames.add(exchange);
-		       allGames.add(oneGames);
+                oneGames.add(hand);
+                oneGames.add(init);
+                oneGames.add(exchange);
+                allGames.add(oneGames);
 
-		    }
-//		    System.out.println(hand);
-//            System.out.println(hand.get(0) + "9999999999999999999");
-//		    System.out.println(init);
-//		    System.out.println(exchange);
-//		    System.out.println(oneGames);
-		    System.out.println(allGames);
+                System.out.println("this is all games");
+                System.out.println(allGames);
+            }
 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-
-	}
+    }
 
     public static int convert(String a) {
         if (a.length() == 3) {
@@ -113,29 +99,24 @@ public class Poker {
         } else {
             if (a.charAt(1) == 74) {
                 return 11;
-            }
-            else if (a.charAt(1) == 81) {
+            } else if (a.charAt(1) == 81) {
                 return 12;
-            }
-            else if (a.charAt(1) == 75) {
+            } else if (a.charAt(1) == 75) {
                 return 13;
-            }
-            else if (a.charAt(1) == 65) {
+            } else if (a.charAt(1) == 65) {
                 return 1;
-            }
-            else{
+            } else {
                 int k = a.charAt(1) - 48;
-                return  k;
+                return k;
             }
         }
     }
 
 
+    public static void main(String[] args) {
+        readCard();
 
-	public static void main(String[] args){
-		readCard();
 
-
-	}
+    }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Test {
 
 
@@ -26,20 +28,36 @@ public class Test {
 
 
     public static void main(String[] args) {
-        String a = "A10";
-        String b = "AJ";
-        String c = "AQ";
-        String d = "AK";
-        String e = "AA";
+//        String a = "A10";
+//        String b = "AJ";
+//        String c = "AQ";
+//        String d = "AK";
+//        String e = "AA";
+//
+//
+//        System.out.println(convert(a));
+//        System.out.println(convert(b));
+//        System.out.println(convert(c));
+//        System.out.println(convert(d));
+//        System.out.println(convert(e));
 
+    int k =90;
+        ArrayList a = new ArrayList();
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(1);
+        a.add(k);
 
-        System.out.println(convert(a));
-        System.out.println(convert(b));
-        System.out.println(convert(c));
-        System.out.println(convert(d));
-        System.out.println(convert(e));
-
-
+        System.out.println(a);
+        k = 0;
+        System.out.println(a);
+        ArrayList b = new ArrayList();
+        b.add(a);
+        b.add(a);
+        System.out.println(b);
+        a.clear();
+        System.out.println(b);
     }
 
 
