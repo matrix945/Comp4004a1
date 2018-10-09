3bcc4f0de772640256f0cf1c5fd8602c936c33f5
matrix945 <frankzhang945@gmail.com>
2018-10-09 04:15:41 -0400

    a little

--- src/Poker.java
+++ src/Poker.java
@@ -8,24 +8,160 @@ import java.util.Collections;
 public class Poker {
 
     public static boolean checkStraight(ArrayList<Card> t) {
-
-        //check the suit not same suit
-        for (int i = 0; i < 5; i++) {
-            if (t.get(0).suit != t.get(i).suit) {
-                return false;
-            }
-        }
-
         //bug for handle A 13 12 11 10 9
         for (int i = 0; i < 5; i++) {
             if (t.get(0).rank != t.get(i).rank + i) {
                 return false;
             }
         }
-
-
         return true;
     }
+
+    //same colour
+    public static boolean checkFlush(ArrayList<Card> t) {
+//        check the suit not same suit
+        for (int i = 0; i < 5; i++) {
+            if (t.get(0).suit != t.get(i).suit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Full house: same 3+2
+    public static boolean checkFullHouse(ArrayList<Card> t) {
+
+        if ( (t.get(1).rank == t.get(2).rank) && (t.get(3).rank == t.get(4).rank) && (t.get(4).rank == t.get(5).rank) ){
+            return true;
+        }
+        if ( (t.get(4).rank == t.get(5).rank) && (t.get(1).rank == t.get(2).rank) && (t.get(2).rank == t.get(3).rank) ){
+            return true;
+        }
+        return false;
+    }
+
+    //Four of a kind: 4+1
+    public static boolean checkFourOfAKind(ArrayList<Card> t) {
+
+    if ( (t.get(0).suit == t.get(1).suit) && (t.get(1).suit == t.get(2).suit) && (t.get(2).suit == t.get(3).suit)
+                && (t.get(3).suit == t.get(4).suit) ){
+        return true;
+    }
+        if ( (t.get(1).suit == t.get(2).suit) && (t.get(2).suit == t.get(3).suit) && (t.get(3).suit == t.get(4).suit)
+                && (t.get(4).suit == t.get(5).suit) ){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean betterThanStraight(ArrayList<Card> t) {
+        return ( checkStraight(t) || checkFlush(t) || checkFourOfAKind(t) || checkFullHouse(t) );
+    }
+
+    //AABBC
+    //AAABC
+    public static boolean oneAwayFullHouse(ArrayList<Card> t) {
+        //AAABC
+        if ((t.get(0).rank == t.get(1).rank) && (t.get(1).rank == t.get(2).rank) ){
+            return true;
+        }
+        //BAAAC
+        if ((t.get(1).rank == t.get(2).rank) && (t.get(2).rank == t.get(3).rank) ){
+            return true;
+        }
+        //BCAAA
+        if ((t.get(2).rank == t.get(3).rank) && (t.get(3).rank == t.get(4).rank) ){
+            return true;
+        }
+
+        //AABBC
+        if ((t.get(0).rank == t.get(1).rank) && (t.get(2).rank == t.get(3).rank) ){
+            return true;
+        }
+        //AACBB
+        if ((t.get(0).rank == t.get(1).rank) && (t.get(3).rank == t.get(4).rank) ){
+            return true;
+        }
+        //CAABB
+        if ((t.get(1).rank == t.get(2).rank) && (t.get(3).rank == t.get(4).rank) ){
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean oneAwayFlush(ArrayList<Card> t){
+        int counter =0;
+        int position=0;
+        for (int i = 0; i < 5; i++) {
+            if (t.get(0).suit != t.get(i).suit) {
+                counter = counter +1;
+                position = i;
+            }
+        }
+        if (counter == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    //one away from straight 12349
+    //bug didn't handle 14567
+    public boolean oneAwayStraight(ArrayList<Card> t){
+        int counter =0;
+        int position=0;
+        for (int i = 0; i < 5; i++) {
+            if (t.get(0).rank != t.get(i).rank + i) {
+                counter = counter +1 ;
+                position = i;
+            }
+        }
+        if (counter ==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    //3 cards same rank AAAXX
+    public boolean threeSameRank(ArrayList<Card> t){
+        //AAABC
+        if ((t.get(0).rank == t.get(1).rank) && (t.get(1).rank == t.get(2).rank) ){
+            return true;
+        }
+        //BAAAC
+        if ((t.get(1).rank == t.get(2).rank) && (t.get(2).rank == t.get(3).rank) ){
+            return true;
+        }
+        //BCAAA
+        if ((t.get(2).rank == t.get(3).rank) && (t.get(3).rank == t.get(4).rank) ){
+            return true;
+        }
+        return false;
+    }
+
+    //three card in a seq
+    public boolean threeInSeq(ArrayList<Card> t){
+       if ( (t.get(0).rank == t.get(1).rank + 1) && (t.get(1).rank == t.get(2).rank + 1) ){
+           return true;
+       }
+        if ( (t.get(1).rank == t.get(2).rank + 1) && (t.get(2).rank == t.get(3).rank + 1) ){
+            return true;
+        }
+
+        if ( (t.get(2).rank == t.get(3).rank + 1) && (t.get(3).rank == t.get(4).rank + 1) ){
+            return true;
+        }
+        return false;
+    }
+
+
 
 
     public boolean win(int a, int b) {
--- src/PokerTest.java
+++ src/PokerTest.java
@@ -1,37 +1,39 @@
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class PokerTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
-	
-	
-	@Test
-	public void winTest() {
-		
-		   boolean z=new Poker().win(0, 1);
-		   assertEquals(false, z);
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
 
-		
-		
-	}
-	
-	
-	
-	
-	@After
-	     public void after() {
-	         System.out.println("after");
-	     }
 
-	
-	
-	
+    @Test
+    public void winTest() {
+
+        boolean z = new Poker().win(0, 1);
+        assertEquals(false, z);
+
+        boolean z1 = new Poker().win(0, 1);
+        assertEquals(false, z1);
+
+        boolean z2 = new Poker().win(0, 1);
+        assertEquals(false, z2);
+
+        boolean z3 = new Poker().win(0, 1);
+        assertEquals(false, z3);
+
+    }
+
+
+    @After
+    public void after() {
+        System.out.println("after");
+    }
+
 
 }
